d6aa9a1917247380ed6c9bd97ec2e67bee968e95
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-02-25 23:03:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var i = 0;
 
 fetchData();
 let more = document.querySelector("#showMore");
-more.addEventListener("click", showMore);
+more.addEventListener("click", showBooks);
 
 function showMore() {
     i = i + 25;
     fetchData();
-    console.log(i);
+    i++;
 }
 
 
@@ -25,20 +25,20 @@ function fetchData() {
 
             let myLen = data.entries.length;
             console.log(myLen);
-            showMovies();
+            showBooks();
 
-            function showMovies() {
+            function showBooks() {
                 for (var j = i; j < 25 + i + 1; j++) {
-                    let movie = data.entries[j];
+                    let book = data.entries[j];
                     container.innerHTML +=
                         `<div class="box">
           <div class="moviesDetails">
             <div class="leftDetails">
             
-              <h3>${movie.API}<h3>
+              <h3>${book.API}<h3>
             
-              <h6>${movie.Description}</h6>
-              <p>${movie.Category}</p>
+              <h6>${book.Description}</h6>
+              <p>${book.Category}</p>
             </div>
           </div>
         </div>`;
